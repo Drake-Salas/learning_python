1c1400b32d4996ffc7828b10b66876445846f45d
Drake <djsalas@ucdavis.edu>
2021-04-14 22:47:57 -0700

    commit try 1

--- sumfac.py
+++ sumfac.py
@@ -4,10 +4,15 @@
 # Also, have it compute the factorial of n while you're at it
 # No, you may not use math.factorial()
 # Use the same loop for both calculations
-
+import math
 n = 5
+l = n * 3
+s = 1
+for i in range(1, 6):
+    s = s * i
+    
+print (n, l, s)
 
-# your code goes here
 
 """
 python3 sumfac.py
