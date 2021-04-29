240b56383112f58a91c930553c75953ee711b687
Drake <djsalas@ucdavis.edu>
2021-04-29 00:13:00 -0700

    Commit1

---
+++ birthday.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# You are probably well aware of the 'birthday paradox'
+# https://en.wikipedia.org/wiki/Birthday_problem
+# Let's try simulating it
+
+# You will need a list for the 365 day calendar
+# You will need a set number of people (e.g. 25)
+# During each 'trial' you do the following
+#	Choose a person
+#	Git them a random birthday
+#	Store it in the calendar
+# Once you have stored all birthdays, check to see if any have the same day
+# Do this for many trials to see what the probability of sharing a birthday is
+
+import random
+
+people = 50
+trials = 3
+for i in range(trials):
+    dups = 0
+    calender = [0] * 365
+    for j in range(people):
+        bday = random.randint(0, 364)
+        calender[bday] += 1
+    print(calender)
+    for k in calender:
+        if k > 1: dups += 1
+    print(dups*2/25)
+"""
+python3 birthday.py
+0.571
+"""
+
